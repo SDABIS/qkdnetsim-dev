deb32522502a4bed67f028fa5aa6926445f540b1
ivan <ivan.cillero@udc.es>
2023-03-24 21:58:02 +0100

    cambio necesario para que funcionen los nuevos cambios

--- scratch/qkd_channel_test.cc
+++ scratch/qkd_channel_test.cc
@@ -152,7 +152,7 @@ int main (int argc, char *argv[])
         1048576,    //min
         11324620, //thr
         52428800,   //max
-        52428800     //current    //20485770
+        0     //current    //20485770
     );
    
     //Create graph to monitor buffer changes
@@ -165,7 +165,7 @@ int main (int argc, char *argv[])
         1048576,    //min
         11324620, //thr
         52428800,   //max
-        52428800     //current    //20485770
+        0     //current    //20485770
     );
     
     //Create graph to monitor buffer changes
